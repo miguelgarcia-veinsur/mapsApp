133b8bf825595a109c18aebda82f901e2757e349
VSN\mgarcia <miguel.garcia@veinsur.es>
2024-03-27 18:36:24 +0100

    Clase 291

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+
+import mapboxgl from 'mapbox-gl';
+(mapboxgl as any).accessToken = 'pk.eyJ1IjoibWlndWVsZ2FyY2lhLXZlaW5zdXIiLCJhIjoiY2x1OXQzdnZkMGF4ODJucWs5enA1NHl2ZiJ9._FWdjRcaWK9hNmIwtuMfOQ';
 
 @Component({
   selector: 'pages-full-screen',
   templateUrl: './full-screen-page.component.html',
   styleUrl: './full-screen-page.component.css'
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
 
 }
