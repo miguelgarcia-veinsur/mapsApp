96c91f0a3c9540be3bdac98a6d701a3aaba0a6fe
MaikelNais <sitevale@hotmail.com>
2024-03-28 22:51:03 +0100

    Clase 296

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Map, LngLat, Marker } from 'mapbox-gl';
 
 @Component({
   selector: 'pages-markers',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrl: './markers-page.component.css'
 })
 export class MarkersPageComponent {
+  @ViewChild('map') divMap?: ElementRef;
 
+  public map?: Map;
+  public currentLngLat: LngLat = new LngLat(-74.10380784179445, 4.651165392795477);
+
+  ngAfterViewInit(): void {
+
+    if (!this.divMap) throw 'El elemento HTML no fue encontrado';
+
+    this.map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentLngLat, // starting position [lng, lat]
+      zoom: 13, // starting zoom
+    });
+
+    //const markerHtml = document.createElement('div');
+    //markerHtml.innerHTML = 'Miguel García';
+
+    //const marker = new Marker({element: markerHtml, /*color: 'red'*/})
+    //                   .setLngLat(this.currentLngLat)
+    //                   .addTo(this.map);
+  }
 }
