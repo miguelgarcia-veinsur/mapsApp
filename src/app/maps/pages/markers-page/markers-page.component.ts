75111e9010175fae396a49438167ee4ddfdf9fbb
MaikelNais <sitevale@hotmail.com>
2024-03-29 18:07:37 +0100

    Clase 299

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -67,4 +67,11 @@ export class MarkersPageComponent {
     this.markers[index].marker.remove();
     this.markers.splice(index, 1);
   }
+
+  flyTo(marker: Marker){
+    this.map?.flyTo({
+      zoom: 14,
+      center: marker.getLngLat(),
+    });
+  }
 }
